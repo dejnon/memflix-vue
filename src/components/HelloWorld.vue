d1f25d7ed88cd7567a5ea9f32c4a1974ba4cf2a4
Daniel Kosalla <dejnon@gmail.com>
2021-08-21 15:26:15 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  mounted() {
+    browser.runtime.sendMessage({});
+  },
+  computed: {
+  },
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 20px;
+}
+</style>
